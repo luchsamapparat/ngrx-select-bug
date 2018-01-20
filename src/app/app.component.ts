ae558122a50d3125636a4385dbbb012e2ccea268
Marvin Luchs <marvin.luchs@t-systems.com>
2018-01-20 16:11:50 +0100

    reproduction for MrWolfZ/ngrx-forms #32

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { createFormGroupState, FormGroupState } from 'ngrx-forms';
+import { Store } from '@ngrx/store';
+import { AppState, FormModel } from './app.module';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  formState: Observable<FormGroupState<FormModel>>;
+
+  constructor(
+    private store: Store<AppState>
+  ) {
+    this.formState = this.store.select(state => state.test);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NgrxFormsModule, formGroupReducer, createFormGroupState, FormGroupState } from 'ngrx-forms';
+import { StoreModule, Action } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
+
+export interface FormModel {
+  select: string;
+}
+
+const initialFormState = createFormGroupState<FormModel>('form', {
+  select: 'USD'
+});
+
+export interface AppState {
+  test: FormGroupState<FormModel>;
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(
+      {
+        test: (state = initialFormState, action: Action) => formGroupReducer(state, action)
+      }
+    ),
+
+    // issue occurs when the NgrxFormsModule is imported before FormsModule
+    NgrxFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
